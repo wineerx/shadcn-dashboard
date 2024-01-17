65bfdcb4e117e0578988d38e2131c90473183596
winnerx <deusdamicrosoft@gmail.com>
2024-01-17 10:29:58 -0300

    feat: Commit

---
+++ src/App.tsx
@@ -0,0 +1,140 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Typography } from '@/components/ui/typography';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { DatePickerWithRange as Datepicker } from '@/components/ui/datepicker'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { DollarSign, UsersRound, CreditCard, Activity } from 'lucide-react';
+
+
+function App() {
+
+  return (
+    <>
+      <div className='flex items-center  justify-center h-screen w-screen'>
+        <div className='w-[1300px] h-[817px] rounded-[0.5rem] border border-border/80 '>
+          <header className='flex h-16 items-center px-4 border-b'>
+            <Button variant="ghost" className="flex gap-2 w-[200px] justify-start ">
+              <Avatar className='w-5 h-5'>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              Alicia Koch
+            </Button>
+
+            <Input type="email" placeholder="Email" />
+          </header>
+          <main className='w-full h-full flex-1 space-y-4 p-8 pt-6'>
+            <div className='flex justify-between'>
+              <Typography variant={'h2'}>Dashboard</Typography>
+              <div className='flex gap-2'>
+                <Datepicker />
+                <Button>Download</Button>
+              </div>
+            </div>
+            <section>
+              <Tabs defaultValue="Overview" className="">
+                <TabsList>
+                  <TabsTrigger value="Overview">Overview</TabsTrigger>
+                  <TabsTrigger value="Analytics" disabled>Analytics</TabsTrigger>
+                  <TabsTrigger value="Reports" disabled>Reports</TabsTrigger>
+                  <TabsTrigger value="Notifications" disabled>Notifications</TabsTrigger>
+                </TabsList>
+
+                <TabsContent className='flex flex-wrap w-full mt-4 gap-4' value="Overview">
+                  <div className='flex gap-4'>
+                    <Card className='flex flex-col gap-2 w-[296px] h-[128px]'>
+                      <div className='flex items-center justify-between'>
+                        <Typography variant={'h3'}>Total Revenue</Typography>
+                        <DollarSign size={16} />
+                      </div>
+
+                      <div className='flex items-center justify-between'>
+                        <div className='flex flex-col'>
+                          <Typography variant={'h2'}>$45,231.89</Typography>
+                          <Typography variant={'p'}>+20.1% from last month</Typography>
+                        </div>
+                      </div>
+
+
+                    </Card>
+
+                    <Card className='flex flex-col gap-2 w-[296px] h-[128px]'>
+                      <div className='flex items-center justify-between'>
+                        <Typography variant={'h3'}>Subscriptions</Typography>
+                        <UsersRound size={16} />
+                      </div>
+
+                      <div className='flex items-center justify-between'>
+                        <div className='flex flex-col'>
+                          <Typography variant={'h2'}>+2350</Typography>
+                          <Typography variant={'p'}>+180.1% from last month</Typography>
+                        </div>
+                      </div>
+                    </Card>
+
+                    <Card className='flex flex-col gap-2 w-[296px] h-[128px]'>
+                      <div className='flex items-center justify-between'>
+                        <Typography variant={'h3'}>Sales</Typography>
+                        <CreditCard size={16} />
+                      </div>
+
+                      <div className='flex items-center justify-between'>
+                        <div className='flex flex-col'>
+                          <Typography variant={'h2'}>+12,234</Typography>
+                          <Typography variant={'p'}>+19% from last month</Typography>
+                        </div>
+                      </div>
+
+
+                    </Card>
+
+                    <Card className='flex flex-col gap-2 w-[296px] h-[128px]'>
+                      <div className='flex items-center justify-between'>
+                        <Typography variant={'h3'}>Active Now</Typography>
+                        <Activity size={16} />
+                      </div>
+
+                      <div className='flex items-center justify-between'>
+                        <div className='flex flex-col'>
+                          <Typography variant={'h2'}>+573</Typography>
+                          <Typography variant={'p'}>+20.1% from last month</Typography>
+                        </div>
+                      </div>
+
+
+                    </Card>
+                  </div>
+
+                  <div className='flex flex-wrap'>
+                    <Card className='flex flex-col gap-2 w-[671px] h-[128px]'>
+                      <Typography variant={'h3'}>Overview</Typography>
+                    </Card>
+                  </div>
+
+                </TabsContent>
+
+              </Tabs>
+
+            </section>
+
+
+          </main>
+
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const typographyVariants = cva(
+  "block text-primary", // Base styles for typography
+  {
+    variants: {
+      variant: {
+        default: "text-xl text-muted-foreground",
+        h1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+        h2: "scroll-m-20 text-3xl font-bold tracking-tight ",
+        h3: "tracking-tight text-sm font-medium",
+        h4: "scroll-m-20 text-xl font-semibold tracking-tight",
+        p: "text-xs text-muted-foreground ",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+interface TypographyProps
+  extends VariantProps<typeof typographyVariants> {
+  asChild?: boolean;
+  children?: React.ReactNode;
+}
+
+const Typography = React.forwardRef<HTMLParagraphElement, TypographyProps>(
+  ({ variant, asChild = false, children, ...props }, ref) => { // Include children in the destructuring
+    const Comp = asChild ? Slot : "p";
+    return (
+      <Comp
+        className={cn(typographyVariants({ variant }))}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </Comp>
+    );
+  }
+);
+
+Typography.displayName = "Typography";
+
+export { Typography, typographyVariants };
